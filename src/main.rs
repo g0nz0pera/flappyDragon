da13801506c4b77bf4ceafa04bb928189ae7235f
G0nz0pera <155404796+g0nz0pera@users.noreply.github.com>
2024-01-31 13:16:23 -0300

    adding initial code, with the state struct, the menus and the control of those menus

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,80 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::metadata;
+use bracket_lib::prelude::*;
+
+struct State {
+    mode: GameMode
+}
+
+impl State {
+    fn new() -> Self {
+        State {
+            mode: GameMode::Menu
+        }
+    }
+    fn play(&mut self, ctx: &mut BTerm){
+        // TODO: Fill this stub later
+        self.mode = GameMode::End;
+    }
+
+    fn restart(&mut self) {
+        self.mode = GameMode::Playing;
+    }
+
+
+    fn main_menu(&mut self, ctx: &mut BTerm){
+        ctx.cls();
+        ctx.print_centered(5, "Welcome to Flappy Dragon");
+        ctx.print_centered(8, "(P) Play the Game");
+        ctx.print_centered(9, "(Q) Quit the Game");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+
+    fn dead(&mut self, ctx: &mut BTerm){
+        ctx.cls();
+        ctx.print_centered(5, "You Are DEAD!");
+        ctx.print_centered(8, "(P) Play Again");
+        ctx.print_centered(9, "(Q) Quit");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+}
+
+enum GameMode {
+    Menu,
+    Playing,
+    End,
+}
+
+impl GameState for State {
+    /* Our game’s tick() function should act like traffic police,
+        directing program flow based upon the current mode.
+        */
+    fn tick(&mut self, ctx: &mut BTerm) {
+        match self.mode {
+            GameMode::Menu => self.main_menu(ctx),
+            GameMode::End => self.dead(ctx),
+            GameMode::Playing => self.play(ctx),
+        }
+    }
+}
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Flappy Dragon")
+        .build()?;
+
+    main_loop(context, State::new())
 }
